ce78c2f492c1cb127b9aa1760d02ab559055c995
Nafisa <nafisalubaba71@gmail.com>
2024-12-03 18:51:09 +0600

    added-functions

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const port = process.env.PORT || 5000
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174',],
-    credentials: true,
-    optionSuccessStatus: 200,
+  origin: ['http://localhost:5173', 'http://localhost:5174',],
+  credentials: true,
+  optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions))
 app.use(express.json());
@@ -31,6 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const apartmentsCollection = client.db('manageNest').collection('apartments')
+    const userCollection = client.db('manageNest').collection('users');
 
 
     app.get('/apartments', async (req, res) => {
@@ -43,21 +44,51 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await apartmentsCollection.findOne(query);
       res.send(result);
-  });
-  app.put('/apartments/:id', async (req, res) => {
-    const id = req.params.id;
-    const updatedData = req.body;
-    const query = { _id: new ObjectId(id) };
-    const update = { $set: updatedData };
-    const result = await apartmentsCollection.updateOne(query, update);
-    res.send(result);
-});
+    });
+    app.put('/apartments/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = { $set: updatedData };
+      const result = await apartmentsCollection.updateOne(query, update);
+      res.send(result);
+    });
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email
+      const result = await userCollection.findOne({ email })
+      res.send(result)
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
 
 
-  
+
     // Connect the client to the server	(optional starting in v4.7)
     // 
-    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -70,11 +101,11 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('website is running')
-  })
-  
-  app.listen(port, () => {
-    console.log(`website is running on port ${port}`)
-  })
+  res.send('website is running')
+})
+
+app.listen(port, () => {
+  console.log(`website is running on port ${port}`)
+})
 
 
